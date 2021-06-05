d86b6d531a1bcd6666926ffe21b4b4dcff8584b0
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-05 21:02:05 +0530

    Control structures should be learnt before Loops.

--- 001-Core-Java/src/main/java/me/ujjawal/learning_java/core/package-info.java
+++ 001-Core-Java/src/main/java/me/ujjawal/learning_java/core/package-info.java
@@ -6,11 +6,11 @@
  *     <li>{@link me.ujjawal.learning_java.core.Constants}</li>
  *     <li>{@link me.ujjawal.learning_java.core.Types}</li>
  *     <li>{@link me.ujjawal.learning_java.core.Operators}</li>
+ *     <li>{@link me.ujjawal.learning_java.core.IfElse}</li>
+ *     <li>{@link me.ujjawal.learning_java.core.SwitchCase}</li>
  *     <li>{@link me.ujjawal.learning_java.core.ForLoop}</li>
  *     <li>{@link me.ujjawal.learning_java.core.WhileLoop}</li>
  *     <li>{@link me.ujjawal.learning_java.core.DoWhileLoop}</li>
- *     <li>{@link me.ujjawal.learning_java.core.IfElse}</li>
- *     <li>{@link me.ujjawal.learning_java.core.SwitchCase}</li>
  *     <li>{@link me.ujjawal.learning_java.core.Functions}</li>
  * </ol>
  */
