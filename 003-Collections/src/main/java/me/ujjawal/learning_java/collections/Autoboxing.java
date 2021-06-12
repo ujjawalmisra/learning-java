646ed4a391cd55f9dbc1bae7ce84347f0cf231d2
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-12 20:54:09 +0530

    Added Autoboxing and Unboxing

--- 003-Collections/src/main/java/me/ujjawal/learning_java/collections/Autoboxing.java
+++ 003-Collections/src/main/java/me/ujjawal/learning_java/collections/Autoboxing.java
@@ -1,4 +1,24 @@
 package me.ujjawal.learning_java.collections;
 
+/**
+ * The automatic conversion done by Java from a primitive type to its wrapper type.
+ */
 public class Autoboxing {
+    public static void main(String[] args) {
+        byte b = 1;
+        Byte bObj = b;
+        System.out.println("b --> " + b);
+        System.out.println("bObj --> " + bObj);
+
+        int x = 1, y = 2;
+        Integer sum = sum(x, y);
+        System.out.println(String.format("sum(%d,%d) --> %d", x, y, sum));
+    }
+
+    private static Integer sum(Integer a, Integer b) {
+        Integer sum = 0;
+        sum += (null != a) ? a : 0;
+        sum += (null != b) ? b : 0;
+        return sum;
+    }
 }
--- 003-Collections/src/main/java/me/ujjawal/learning_java/collections/Unboxing.java
+++ 003-Collections/src/main/java/me/ujjawal/learning_java/collections/Unboxing.java
@@ -1,4 +1,21 @@
 package me.ujjawal.learning_java.collections;
 
+/**
+ * The automatic conversion done by Java from a wrapper type to its primitive type.
+ */
 public class Unboxing {
+    public static void main(String[] args) {
+        Byte bObj = 1;
+        byte b = bObj;
+        System.out.println("bObj --> " + bObj);
+        System.out.println("b --> " + b);
+
+        Integer x = 1, y = 2;
+        int sum = sum(x, y);
+        System.out.println(String.format("sum(%d,%d) --> %d", x, y, sum));
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
 }
