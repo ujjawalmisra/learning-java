6e20c66349f5c854f2128bacfd7b3e439959fcd9
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-14 22:54:04 +0530

    Completed TheArrays

--- 003-Collections/src/main/java/me/ujjawal/learning_java/collections/TheArrays.java
+++ 003-Collections/src/main/java/me/ujjawal/learning_java/collections/TheArrays.java
@@ -1,4 +1,35 @@
 package me.ujjawal.learning_java.collections;
 
+import java.util.Arrays;
+
 public class TheArrays {
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5};//Integer
+        for(int n : numbers) {
+            System.out.println(n);
+        }
+
+        System.out.println(Arrays.stream(numbers).sum());
+        System.out.println(Arrays.stream(numbers).min().getAsInt());
+        System.out.println(Arrays.stream(numbers).max().getAsInt());
+
+        /**
+         * Data-Structures
+         * 1. Linked-List -- add, remove, insert, next
+         * 2. Stack -- pop, push
+         * 3. Queue -- enqueue, dequeue
+         * 4. Tree
+         * 5. Heap
+         * 6. Hash
+         * 7. Graphs
+         * 8. Simple Array
+         *
+         * Operations:
+         * 1. Modifying the data-structure -- adding, removing
+         * 2. Search -- find
+         * 3. Traverse -- iterate
+         */
+
+    }
 }
