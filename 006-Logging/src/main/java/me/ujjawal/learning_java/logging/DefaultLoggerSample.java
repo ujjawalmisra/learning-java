dec9d55af84e372374777f00f6cbef33a30b1283
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-16 15:05:10 +0530

    Simple logging Sample using java.util.logging. Some methods aren't working as expected -- fine, finer, finest.

--- 006-Logging/src/main/java/me/ujjawal/learning_java/logging/DefaultLoggerSample.java
+++ 006-Logging/src/main/java/me/ujjawal/learning_java/logging/DefaultLoggerSample.java
@@ -10,12 +10,26 @@ public class DefaultLoggerSample {
     public static void main(String[] args) {
         System.out.println("This is a message from System.out");
         logger.info("This is a message from logger.info");
+        logger.warning("This is a message from logger.warning");
+        logger.setLevel(Level.ALL);
         for(Level level : new Level[] {Level.OFF, Level.SEVERE, Level.WARNING, Level.INFO, Level.CONFIG, Level.FINE,
                 Level.FINER, Level.FINEST, Level.ALL}) {
 
             logger.info(String.format("Level --> %s, %d, %b", level.getName(), level.intValue(),
                     logger.isLoggable(level)));
         }
+        logger.severe("This is a message from logger.severe");
+        logger.info("level: " + logger.getLevel().getName());
+        int sum = sum(1,2);
+        logger.info("sum:" + sum);
 
+    }
+
+    private static int sum(int x, int y) {
+        logger.finest("x:" + x);
+        logger.info("y:" + y);
+        int result = x + y;
+        logger.info("result:" + result);
+        return result;
     }
 }
