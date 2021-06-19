d17ac56dbf0756bcceeab72b62bd9929a8a4f082
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-19 22:26:40 +0530

    updated docs for PredefinedAnnotations

--- 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/PredefinedAnnotations.java
+++ 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/PredefinedAnnotations.java
@@ -10,6 +10,18 @@ import java.util.Arrays;
  *     <li>{@link SafeVarargs} -- Mark that the code only uses variadic arguments in a safe fashion</li>
  *     <li>{@link FunctionalInterface} -- Check the example on Basic Unit Testing</li>
  * </ol>
+ * Some of the scenarios that can be suppressed are:
+ * <ol>
+ *     <li>deprecation -- Ignores when we’re using a deprecated(no longer important) method or type</li>
+ *     <li>divzero -- Suppresses division by zero warning</li>
+ *     <li>empty -- Ignores warning of a statement with an empty body</li>
+ *     <li>unchecked -- It doesn’t check if the data type is Object or primitive</li>
+ *     <li>fallthrough -- Ignores fall-through on switch statements usually (if <code>break</code> is missing)</li>
+ *     <li>hiding -- It suppresses warnings relative to locals that hide variable</li>
+ *     <li>serial -- It makes the compiler shut up about a missing serialVersionUID</li>
+ *     <li>finally -- Avoids warnings relative to finally block that doesn’t return</li>
+ *     <li>unused -- To suppress warnings relative to unused code.</li>
+ * </ol>
  * There are some annotations that can be applied to other annotations. In other words when we define custom
  * annotations we may use these pre-defined annotations for annotation the newly defined custom annotation.
  * <ol>
