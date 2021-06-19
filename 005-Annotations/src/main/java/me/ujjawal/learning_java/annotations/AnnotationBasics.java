a3bd43232305aae23078d961b1da4685c25539ac
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-06-19 17:54:49 +0530

    Added basic and predefined annotations

--- 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/AnnotationBasics.java
+++ 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/AnnotationBasics.java
@@ -1,4 +1,35 @@
 package me.ujjawal.learning_java.annotations;
 
+/**
+ * Annotations provided added details or metadata about a class / interface / function etc. These details or
+ * information might be relevant for:<br>
+ * <ol>
+ *     <li>Compiler -- Compile-time validation or suppression of errors.</li>
+ *     <li>Runtime -- Runtime validations.</li>
+ *     <li>Source-generation -- Compile-time or Runtime generation of code or files etc.</li>
+ * </ol>
+ */
 public class AnnotationBasics {
+
+    public static void main(String[] args) {
+        /*
+         * One of the most common annotation which we use is @Override
+         */
+        Animal animal = new Dog();
+        animal.makeSound();
+
+        Dog dog = (Dog) animal;
+    }
+
+    private static abstract class Animal {
+        abstract void makeSound();
+    }
+
+    private static class Dog extends Animal {
+
+        @Override
+        void makeSound() {
+            System.out.println("bark");
+        }
+    }
 }
--- 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/PredefinedAnnotations.java
+++ 005-Annotations/src/main/java/me/ujjawal/learning_java/annotations/PredefinedAnnotations.java
@@ -1,4 +1,52 @@
 package me.ujjawal.learning_java.annotations;
 
+import java.util.Arrays;
+
+/**
+ * Some of the predefined annotations are:
+ * <ol>
+ *     <li>{@link Deprecated} -- Mark the method as obsolete</li>
+ *     <li>{@link SuppressWarnings} -- Indicate to the compiler that specific warnings can be suppressed</li>
+ *     <li>{@link SafeVarargs} -- Mark that the code only uses variadic arguments in a safe fashion</li>
+ *     <li>{@link FunctionalInterface} -- Check the example on Basic Unit Testing</li>
+ * </ol>
+ * There are some annotations that can be applied to other annotations. In other words when we define custom
+ * annotations we may use these pre-defined annotations for annotation the newly defined custom annotation.
+ * <ol>
+ *     <li>{@link java.lang.annotation.Retention} -- SOURCE (dev-time) or CLASS (compile-time) or RUNTIME (runtime)</li>
+ *     <li>{@link java.lang.annotation.Documented} -- Document the elements when custom annotation is used</li>
+ *     <li>{@link java.lang.annotation.Target} -- What kind of Java elements can the custom annotation be used with</li>
+ *     <li>{@link java.lang.annotation.Inherited} -- Marks custom annotation inheritable from a super class</li>
+ *     <li>{@link java.lang.annotation.Repeatable} -- Can be applied multiple times to the same element</li>
+ * </ol>
+ */
 public class PredefinedAnnotations {
+
+    public static void main(String[] args) {
+        System.out.println("sum --> " + sum(1, 2, 3, 4));
+    }
+
+    @SafeVarargs
+    private static int sum(int... x) {
+        return Arrays.stream(x).sum();
+    }
+
+    private static class Animal {
+
+        @Deprecated
+        private String getSound() {
+            return "argh";
+        }
+
+        @SuppressWarnings("deprecation")
+        private void oldMakeSound() {
+            System.out.println(getSound());
+        }
+
+        @Override
+        public String toString() {
+            return "Animal{}";
+        }
+    }
+
 }
