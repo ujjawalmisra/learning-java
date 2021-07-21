bc1545cf990c7f1fff654b3018de3c129baed5f7
Ujjawal Misra <ujjawal.misra@zopsmart.com>
2021-07-21 20:16:23 +0530

    added a class for demo of ClassNotFoundException

---
+++ 007-Exception-Handling/src/main/java/me/ujjawal/learning_java/exceptions/CNFSample.java
@@ -0,0 +1,16 @@
+package me.ujjawal.learning_java.exceptions;
+
+/**
+ * Demo of ClassNotFoundException.
+ * Note: Added this class to resources j
+ */
+public class CNFSample {
+    public static void main(String[] args) {
+        System.out.println("Will try to demo a ClassNotFoundException");
+        /*
+        Uncomment and run with the following lines to demonstrate.
+         */
+//        SomeUnknownClass obj = new SomeUnknownClass();
+//        System.out.println("SomeUnknownClass obj --> " + obj);
+    }
+}
--- 007-Exception-Handling/src/main/resources/CNFSample.java
+++
@@ -1,7 +0,0 @@
-public class CNFSample {
-    public static void main(String[] args) {
-        System.out.println("Will try to demo a ClassNotFoundException");
-        SomeUnknownClass obj = new SomeUnknownClass();
-        System.out.println("SomeUnknownClass obj --> " + obj);
-    }
-}
